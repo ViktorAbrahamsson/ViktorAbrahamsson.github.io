a22b0bdc304cab0b2f8bd91171dfa3e15ba89502
Viktor Abrahamsson <wicke.abrahamsson@gmail.com>
2020-04-21 22:51:54 +0200

    Burger-menu now working
    Next up: Get navigation up and going when burger is clicked

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const navButton = document.querySelector(".burger");
+const navBurger = document.querySelector(".burger");
 
-const toggleNav = () => {};
+const toggleNav = (e) => {
+  if (!e.target.classList.contains("active")) {
+    // Open Nav
+    e.target.classList.add("active");
+    // Hide rest of body
+    document.body.classList.add("hide");
+    // GSAP Animations
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(1500px at 100% -10%)" });
+  } else {
+    e.target.classList.remove("active");
+    document.body.classList.remove("hide");
+    // GSAP Animations
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+  }
+};
 
 // Event Listeners
-navButton.addEventListener("click", toggleNav());
+navBurger.addEventListener("click", toggleNav);
